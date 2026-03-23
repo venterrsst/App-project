1e0562af38e75bbb79e891171bd517e68f2e6de5
venterrsst <venterrsst@gmail.com>
2026-03-23 20:45:20 +0300

    Изменен интерфейс

--- interface.py
+++ interface.py
@@ -1,13 +1,22 @@
 from tkinter import *
 root = Tk()
 window2 = None
+window3 = None
 hint = "Море"
+
 def close_window(event):
     root.destroy()
+
 def back_to_main_window(event):
     global window2
     window2.destroy()
     root.deiconify()
+
+def back_to_main_window2(event):
+    global window3
+    window3.destroy()
+    root.deiconify()
+
 def open_window(event):
     root.withdraw()
     global window2
@@ -27,13 +36,31 @@ def open_window(event):
     button3 = Button(window2, text="Выйти из игры")
     button3.bind("<Button-1>", back_to_main_window)
     button3.pack()
+
+def game_description(event):
+    root.withdraw
+    global window3
+    window3 = Toplevel(root)
+    window3.attributes("-fullscreen", True)
+    name = Label(window3, text="Описание игры")
+    name.pack()
+    description = Label(window3, text="Крокодил представляет собой однопользовательскую программу,\nгенерирующую рандомные слова, которые игрок должен угадать по ассоциациям.\nВсего дается 3 попытки. Каждая следующая попытка добавляет новую ассоциацию к слову.\nИгра заканчивается, когда игрок угадывает загаданное слово, или когда заканчивается количество попыток.")
+    description.pack()
+    button4 = Button(window3, text="Вернуться в меню")
+    button4.bind("<Button-1>", back_to_main_window2)
+    button4.pack()
+
 root.attributes("-fullscreen", True)
-label = Label(root, text="Крокодил")
+canvas = Canvas(root, width=3000, height=1500, bg="lightblue")
+canvas.create_text(760, 40, text="Крокодил", font=("Arial, 24"))
 button1 = Button(root, text="Начать игру")
+button5 = Button(root, text="Описание игры")
 button2 = Button(root, text="Выйти из игры")
 button2.bind("<Button-1>", close_window)
 button1.bind("<Button-1>", open_window)
-label.pack()
-button1.pack()
-button2.pack()
+button5.bind("<Button-1>", game_description)
+button1.place(x=20, y=60)
+button5.place(x=20, y=80)
+button2.place(x=20, y=100)
+canvas.place(x=0, y=0)
 root.mainloop()
